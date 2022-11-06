b4eedbd5558ff4432997ab193002ea6ae05d726e
user@chinonsopc <Nonsoacc1@yahoo.com>
2022-11-05 23:29:53 -0600

    bug-fix new demo 123

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -9,12 +9,11 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 
-		String name = "Nonso";
+		String name = "nonso";
 
 		System.out.println(name);
 
 		Integer age = "20";
-
 		System.out.println(age+ 5);
 		
 	}
